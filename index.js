b6db9d691aded3d06ba6b4aad1d1a9fc4681f048
ypawan83 <99489694+ypawan83@users.noreply.github.com>
2022-08-08 20:13:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+
+questionEl.innerText = `What is ${num1} multiply by ${num2} `;
+
+const correctAns = num1 * num2;
+let score = Number( localStorage.getItem("score"));
+if(!score){
+    score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+
+formEl.addEventListener("submit", ()=>{
+    const userAns = +inputEl.value;
+    if(userAns === correctAns){
+        score++;
+        updateLocalStorage();
+    }else{
+        score--;
+        updateLocalStorage();
+    }
+});
+
+function updateLocalStorage(){
+    localStorage.setItem("score", score.toString());
+}
